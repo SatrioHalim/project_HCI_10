635c7f27b7d84ca2a83d42da3d160f43b337ce05
SatrioHalim <satriohalim21@gmail.com>
2024-06-16 01:13:09 +0700

    16/06/2024

---
+++ favoritenotes.js
@@ -0,0 +1,12 @@
+function overlay1(){
+    let overlay = document.getElementById("overlay1");
+    overlay.classList.toggle("active");
+}
+function overlay2(){
+    let overlay = document.getElementById("overlay2");
+    overlay.classList.toggle("active");
+}
+function overlay3(){
+    let overlay = document.getElementById("overlay3");
+    overlay.classList.toggle("active");
+}
